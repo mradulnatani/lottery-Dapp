554034d2477a5ea6e97f034973cc1fb26dc9eff8
mradul_09 <mradulnatani0@gmail.com>
2024-06-07 00:12:06 +0530

    removed argument statement

--- deploy.js
+++ deploy.js
@@ -17,7 +17,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
  
   const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .deploy({ data: evm.bytecode.object})
     .send({ gas: '1000000', from: accounts[0] });
  
   console.log('Contract deployed to', result.options.address);
