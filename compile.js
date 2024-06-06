415b4607b80779c912083ba9788f34605d196a5d
mradul_09 <mradulnatani0@gmail.com>
2024-06-07 00:02:18 +0530

    modifies compile.js

--- compile.js
+++ compile.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
-const filetPath = path.resolve(__dirname,'contracts','lottery.sol');
+const filePath = path.resolve(__dirname,'contracts','lottery.sol');
 const source = fs.readFileSync(filePath,'utf8');
-module.exports = solc.compile(source,1);
+module.exports =solc.compile(source,1);
