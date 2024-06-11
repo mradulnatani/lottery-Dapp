1fd10833bc130246a928e4240a570677461a3075
mradul_09 <mradulnatani0@gmail.com>
2024-06-11 23:53:43 +0530

    Modified test, deploy and compile files

--- compile.js
+++ compile.js
@@ -1,10 +1,13 @@
 
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
-const lotteryPath = path.resolve(__dirname,'contracts','lottery.sol');
-const source = fs.readFileSync(lotteryPath,'utf8');
-module.exports =solc.compile(source,1).contracts[':lottery'];
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8");
+
+module.exports = solc.compile(source, 1).contracts[":Lottery"];
+
 /*
 const path = require('path');
 const fs = require('fs');
