c3555002d13c8513ec5e3eb2cafaa207498b02ed
mradul_09 <mradulnatani0@gmail.com>
2024-06-20 12:24:01 +0530

    Added some details to form

--- lottery/src/main.js
+++ lottery/src/main.js
@@ -5,8 +5,9 @@ export default function main() {
         e.preventDefault();
     }
   return (
-    <div>
+    <div style={{border:"10px"}}>
       <br/>
+      <div>
       <span style={{padding:"100px"}}>Want to earn some heavy money?</span>
       <br/>
       <spam style={{padding:"100px"}}>If Yes then here is a chance......</spam>
@@ -14,11 +15,18 @@ export default function main() {
       <b style={{marginLeft:"100px",border:"10px"}}>Disclimer: This application is managed by the manager of the D-App company, all the care of you privacy and security will be taken care during the game.</b>
       <br/>
       <br/>
-      <form style={{marginLeft:"200px"}}>
-        <br/>
-        Enter amount of Ether:<input type='text'></input>
-        <button type='submit' onClick={getData} style={{margin:"10px", width:"100px"}}>Enter</button>
-      </form>
+      </div>
+      <form style={{margin:"10px 500px 10px 500px"}}>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Amount</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" for="exampleCheck1">Accept the terms and conditions</label>
+  </div>
+  <button type="submit" className="btn btn-primary" onClick={getData}>Enter the lottery</button>
+</form>
     </div>
   )
 }
